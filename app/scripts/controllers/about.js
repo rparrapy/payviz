a236c4d8f5958666edc13aa913181bc75bc1f6c7
Rodrigo Parra <rodpar07@gmail.com>
2014-12-24 11:52:28 -0300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name payvizApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the payvizApp
+ */
+angular.module('payvizApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
